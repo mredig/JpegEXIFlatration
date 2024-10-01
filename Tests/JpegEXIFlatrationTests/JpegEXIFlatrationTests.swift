047268cee25ecaa79f90fda86082ab9bdc651ed9
Michael Redig <mredig@gmail.com>
2024-10-01 16:45:46 -0500

    (refactor) tests actually test something

--- Tests/JpegEXIFlatrationTests/JpegEXIFlatrationTests.swift
+++ Tests/JpegEXIFlatrationTests/JpegEXIFlatrationTests.swift
@@ -6,12 +6,12 @@ import exif
 
 
 @Test
-func testRawExifFunctions() async throws {
+func testFileInit() async throws {
 	let imageURL = Bundle.module.url(forResource: "Canon_40D", withExtension: "jpg")!
 
-	let userData = try EXIFlatratedData(fileURL: imageURL)
+	let data = try EXIFlatratedData(fileURL: imageURL)
 
-	print(userData)
+	try verifyCanon40d(data)
 }
 
 @Test
@@ -19,9 +19,21 @@ func testDataInit() async throws {
 	let imageURL = Bundle.module.url(forResource: "Canon_40D", withExtension: "jpg")!
 	let imageData = try Data(contentsOf: imageURL)
 
-	let userData = try EXIFlatratedData(imageData: imageData)
+	let data = try EXIFlatratedData(imageData: imageData)
 
-	print(userData)
+	try verifyCanon40d(data)
+}
+
+func verifyCanon40d(_ data: EXIFlatratedData) throws {
+	let expectedIFDs: Set<ExifIfd> = [.ifd0, .ifd1, .exif, .gps, .interoperability]
+	#expect(Set(data.dictionary.keys) == expectedIFDs)
+
+	// sample parts
+	#expect(data.dictionary[.ifd0]?["Orientation"] == "Top-left")
+	#expect(data.dictionary[.ifd1]?["Compression"] == "JPEG compression")
+	#expect(data.dictionary[.exif]?["ExposureTime"] == "1/160 sec.")
+	#expect(data.dictionary[.gps]?["GPSVersionID"] == "2.2.0.0")
+	#expect(data.dictionary[.interoperability]?["InteroperabilityIndex"] == "R98")
 }
 
 enum TestError: Error {
