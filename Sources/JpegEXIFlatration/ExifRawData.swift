46a6ca2671fbffc6ddf78fd3bdf3f66a31bd05c2
Michael Redig <mredig@gmail.com>
2024-10-02 00:14:01 -0500

    (feat) added various data conversions

--- Sources/JpegEXIFlatration/ExifRawData.swift
+++ Sources/JpegEXIFlatration/ExifRawData.swift
@@ -57,4 +57,173 @@ public struct ExifRawData: Sendable, Hashable {
 	public var asString: String? {
 		String(data: rawData, encoding: .utf8)
 	}
+
+	public var asByte: UInt8? {
+		rawData.first
+	}
+
+	private func toType<T>(from offset: Int = 0, _ type: T.Type = T.self) -> T? {
+		let size = MemoryLayout<T>.size
+
+		let startIndex = rawData.index(rawData.startIndex, offsetBy: offset)
+		let endIndex = rawData.index(startIndex, offsetBy: size)
+
+		let range = startIndex..<endIndex
+		guard
+			rawData.indices.contains(range)
+		else { return nil }
+
+		let section = rawData[range]
+
+		return section.withUnsafeBytes { buffer in
+			buffer.bindMemory(to: T.self).first
+		}
+	}
+
+	public var asShort: UInt16? {
+		guard
+			let base: UInt16 = toType()
+		else { return nil }
+
+		if endianness == .little {
+			return UInt16(littleEndian: base)
+		} else {
+			return UInt16(bigEndian: base)
+		}
+	}
+
+	public var asLong: UInt32? {
+		guard
+			let base: UInt32 = toType()
+		else { return nil }
+
+		if endianness == .little {
+			return UInt32(littleEndian: base)
+		} else {
+			return UInt32(bigEndian: base)
+		}
+	}
+
+	public var asRational: RationalValue<UInt32>? {
+		guard
+			let baseNumerator: UInt32 = toType(),
+			let baseDenominator: UInt32 = toType(from: MemoryLayout<UInt32>.size)
+		else { return nil }
+
+		if endianness == .little {
+			let num = UInt32(littleEndian: baseNumerator)
+			let den = UInt32(littleEndian: baseDenominator)
+			return RationalValue(numerator: num, denominator: den)
+		} else {
+			let num = UInt32(bigEndian: baseNumerator)
+			let den = UInt32(bigEndian: baseDenominator)
+			return RationalValue(numerator: num, denominator: den)
+		}
+	}
+
+	public var asUndefined: Data { rawData }
+
+	public var asSbyte: Int8? { toType() }
+
+	public var asSignedShort: Int16? {
+		guard
+			let base: Int16 = toType()
+		else { return nil }
+
+		if endianness == .little {
+			return Int16(littleEndian: base)
+		} else {
+			return Int16(bigEndian: base)
+		}
+	}
+
+	public var asSignedLong: Int32? {
+		guard
+			let base: Int32 = toType()
+		else { return nil }
+
+		if endianness == .little {
+			return Int32(littleEndian: base)
+		} else {
+			return Int32(bigEndian: base)
+		}
+	}
+
+	public var asSignedRational: RationalValue<Int32>?  {
+		guard
+			let baseNumerator: Int32 = toType(),
+			let baseDenominator: Int32 = toType(from: MemoryLayout<Int32>.size)
+		else { return nil }
+
+		if endianness == .little {
+			let num = Int32(littleEndian: baseNumerator)
+			let den = Int32(littleEndian: baseDenominator)
+			return RationalValue(numerator: num, denominator: den)
+		} else {
+			let num = Int32(bigEndian: baseNumerator)
+			let den = Int32(bigEndian: baseDenominator)
+			return RationalValue(numerator: num, denominator: den)
+		}
+	}
+
+	public var asFloat: Float? {
+		guard
+			let base: UInt32 = toType()
+		else { return nil }
+
+		if endianness == .little {
+			let floatBits = UInt32(littleEndian: base)
+			return Float(bitPattern: floatBits)
+		} else {
+			let floatBits = UInt32(bigEndian: base)
+			return Float(bitPattern: floatBits)
+		}
+	}
+
+	public var asDouble: Double? {
+		guard
+			let base: UInt64 = toType()
+		else { return nil }
+
+		if endianness == .little {
+			let floatBits = UInt64(littleEndian: base)
+			return Double(bitPattern: floatBits)
+		} else {
+			let floatBits = UInt64(bigEndian: base)
+			return Double(bitPattern: floatBits)
+		}
+	}
+}
+
+extension ExifRawData {
+	public struct RationalValue<Num: BinaryInteger & Sendable & Codable & Hashable>: Sendable, Codable, Hashable {
+		public var isValid: Bool { denominator != 0 }
+		public let numerator: Num
+		public let denominator: Num
+
+		public var doubleValue: Double { Double(numerator) / Double(denominator) }
+
+		public var evToShutterSpeed: Double {
+			1 / exp2(doubleValue)
+		}
+
+		static private func greatestCommonDenominator(_ a: Num, b: Num) -> Num {
+			b == 0 ? a : greatestCommonDenominator(b, b: a % b)
+		}
+
+		public func reduced() -> Self {
+			guard
+				isValid
+			else { return self }
+
+			let divisor = Self.greatestCommonDenominator(numerator, b: denominator)
+			guard divisor != 1 else { return self }
+			return Self(numerator: numerator / divisor, denominator: denominator / divisor)
+		}
+
+		public init(numerator: Num, denominator: Num) {
+			self.numerator = numerator
+			self.denominator = denominator
+		}
+	}
 }
--- Tests/JpegEXIFlatrationTests/JpegEXIFlatrationTests.swift
+++ Tests/JpegEXIFlatrationTests/JpegEXIFlatrationTests.swift
@@ -28,10 +28,19 @@ func verifyCanon40d(_ data: EXIFlatratedData) throws {
 	let expectedIFDs: Set<ExifIfd> = [.ifd0, .ifd1, .exif, .gps, .interoperability]
 	#expect(Set(data.dictionary.keys) == expectedIFDs)
 
+	typealias Rational = ExifRawData.RationalValue
+
 	// sample parts
 	#expect(data.dictionary[.ifd0]?[.orientation]?.libexifString == "Top-left")
 	#expect(data.dictionary[.ifd1]?[.compression]?.libexifString == "JPEG compression")
 	#expect(data.dictionary[.exif]?[.exposureTime]?.libexifString == "1/160 sec.")
+	#expect(data.dictionary[.exif]?[.exposureTime]?.asRational == Rational(numerator: 1, denominator: 160))
+	#expect(data.dictionary[.exif]?[.focalPlaneResolutionUnit]?.asShort == 2)
+	#expect(data.dictionary[.exif]?[.apertureValue]?.asRational == Rational(numerator: 368640, denominator: 65536))
+	#expect(data.dictionary[.exif]?[.apertureValue]?.asRational?.reduced() == Rational(numerator: 45, denominator: 8))
+	#expect(data.dictionary[.exif]?[.apertureValue]?.libexifString == "5.62 EV (f/7.0)")
+	#expect(data.dictionary[.exif]?[.shutterSpeedValue]?.asSignedRational == Rational(numerator: 483328, denominator: 65536))
+	#expect(data.dictionary[.exif]?[.shutterSpeedValue]?.asSignedRational?.reduced() == Rational(numerator: 59, denominator: 8))
 	#expect(data.dictionary[.gps]?[.gpsVersionId]?.libexifString == "2.2.0.0")
 	#expect(data.dictionary[.interoperability]?[.interoperabilityIndex]?.libexifString == "R98")
 }
